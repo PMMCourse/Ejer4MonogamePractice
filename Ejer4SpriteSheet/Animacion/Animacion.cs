49f472237a6105d18bd15d920c8a4718470f471a
joaquinBB99 <joaquin.bejarano.brenes@alumnos.fesac.es>
2019-10-24 13:04:54 +0200

    PRMonoGame

---
+++ Ejer4SpriteSheet/Animacion/Animacion.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejer4SpriteSheet.Animacion
+{
+    public class Animacion
+    {
+        public int Frame { get; set; }
+        public int ContadorFrame { get; set; }
+        public int VelocidadFrame { get; set; }
+        public int velocidadFrame { get; set; }
+
+        public Animacion(Texture2D texture, int contadorFrame)
+        {
+            Texture = texture;
+
+            ContadorFrame = contadorFrame;
+
+
+        }
+
+    }
+}
